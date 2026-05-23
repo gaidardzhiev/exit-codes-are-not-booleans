e988cd664af25b2502744e20eb7f197e8635a958
gaidardzhiev <1v4n@protonmail.com>
2026-05-23 07:59:09 +0000

    add pdf generation shell script

---
+++ tex2pdf.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+flatex() {
+	printf "do you want to build the LaTeX document form source? (y/n)\n"
+	read X
+	case "${X}" in
+		[y]*)
+			printf "building LaTeX from source...\nplease choose pdflatex or xelatex:\n"
+			while :; do
+				read Y
+				case "${Y}" in
+					xelatex)
+						xelatex "${1}" || return 8
+						cp "${1%.tex}.pdf" /home/tower/Downloads || return 16
+						break
+						;;
+					pdflatex)
+						pdflatex "${1}" || return 32
+						cp "${1%.tex}.pdf" /home/tower/Downloads || return 64
+						break
+						;;
+					*)
+						printf "invalid response...\nchoose: xelatex/pdflatex\n"
+						;;
+				esac
+			done
+			;;
+		[n]*)
+			printf "you chose NOT to build LaTeX from source...\n"
+			;;
+		*)
+			printf "invalid response...\n"
+			printf "(yes/no)\n"
+			;;
+	esac
+}
+
+{ flatex; RET="${?}"; } || exit 1
+
+[ "${RET}" -eq 0 ] 2>/dev/null || printf "%s\n" "${RET}"
